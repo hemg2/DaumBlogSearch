314ba31972ebc92536fd7b97d2f397f7afbd4d25
hemg2 <hemg2@naver.com>
2023-01-05 15:58:16 +0900

    window RootView

---
+++ SearchBlog/View/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  SearchBlog
+//
+//  Created by 1 on 2023/01/05.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+
+final class MainViewContoller: UIViewController {
+    
+}
