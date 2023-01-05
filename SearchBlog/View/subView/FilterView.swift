0de96d6dd874564afc01177f13d6cdcb4df708d2
hemg2 <hemg2@naver.com>
2023-01-05 16:49:07 +0900

    FilterView

---
+++ SearchBlog/View/subView/FilterView.swift
@@ -0,0 +1,66 @@
+//
+//  FilterView.swift
+//  SearchBlog
+//
+//  Created by 1 on 2023/01/05.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+final class FilterView: UITableViewHeaderFooterView {
+    let disposeBag = DisposeBag()
+    
+    
+    let sortButton = UIButton()
+    let bottomBorder = UIView()
+    
+    // 필터뷰 외부에서 관찰
+    let sortButtonTapped = PublishRelay<Void>()
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        
+        bind()
+        attribute()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    private func bind() {
+        sortButton.rx.tap
+            .bind(to: sortButtonTapped)
+            .disposed(by: disposeBag)
+    }
+    
+    private func attribute() {
+        sortButton.setImage(UIImage(systemName: "list.bullet"), for: .normal)
+        bottomBorder.backgroundColor = .lightGray
+    
+    }
+    
+    private func layout() {
+        [sortButton, bottomBorder].forEach { addSubview($0)
+            
+        }
+        
+        sortButton.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(12)
+            $0.width.height.equalTo(28)
+            
+        }
+        
+        bottomBorder.snp.makeConstraints {
+            $0.top.equalTo(sortButton.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.height.equalTo(0.5)
+        }
+    }
+    
+}
