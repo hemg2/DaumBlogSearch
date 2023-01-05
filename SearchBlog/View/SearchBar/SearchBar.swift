17afaee76dd36f6f0e7d7e93a96eecdaf4882881
hemg2 <hemg2@naver.com>
2023-01-05 16:40:59 +0900

    SearchBar

--- SearchBlog/View/SearchBar/SearchBar.swift
+++ SearchBlog/View/SearchBar/SearchBar.swift
@@ -8,12 +8,12 @@
 import UIKit
 import RxSwift
 import RxCocoa
-
+import SnapKit
 
 final class SearchBar: UISearchBar {
     let disposeBag = DisposeBag()
     
-    let serchButton = UIButton()
+    let searchButton = UIButton()
     //서치바 버튼 이벤트
     let searchButtonTapped = PublishRelay<Void>()
     //서치바 외부로 보내는 이벤트
@@ -33,14 +33,44 @@ final class SearchBar: UISearchBar {
     }
     
     private func bind() {
+        Observable.merge(self.rx.searchButtonClicked.asObservable(),
+                         searchButton.rx.tap.asObservable()).bind(to: searchButtonTapped)
+            .disposed(by: disposeBag)
         
+        searchButtonTapped.asSignal().emit(to: self.rx.endEditing)
+            .disposed(by: disposeBag)
+        
+        self.shouldLoadResult = searchButtonTapped
+            .withLatestFrom(self.rx.text) { $1 ?? "" }
+            .filter {!$0.isEmpty }
+            .distinctUntilChanged()
     }
     
     private func attribute() {
-        
+        searchButton.setTitle("검색", for: .normal)
+        searchButton.setTitleColor(.systemBlue, for: .normal)
     }
     
     private func layout() {
+        addSubview(searchButton)
         
+        searchTextField.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(12)
+            $0.trailing.equalTo(searchButton.snp.leading).offset(-12)
+            $0.centerY.equalToSuperview()
+        }
+        searchButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(12)
+        }
+    }
+}
+
+
+extension Reactive where Base: SearchBar {
+    var endEditing: Binder<Void> {
+        return Binder(base) { base, _ in
+            base.endEditing(true)
+        }
     }
 }
