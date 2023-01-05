da1a5e02d57e34996a8deb50b91aa60b9fc5da55
hemg2 <hemg2@naver.com>
2023-01-05 17:21:07 +0900

    BlogList

--- SearchBlog/View/subView/BlogList.swift
+++ SearchBlog/View/subView/BlogList.swift
@@ -5,4 +5,51 @@
 //  Created by 1 on 2023/01/05.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+
+final class BlogListView: UITableView {
+    let disposeBag = DisposeBag()
+    
+    let headerView = FilterView(frame: CGRect(origin: .zero, size: CGSize(width: UIScreen.main.bounds.width, height: 50)))
+    let cellData = PublishSubject<[BlogListCellData]>()
+    
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+        bind()
+        attribute()
+        layout()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func bind() {
+        // cellforRowat 이 데이타 소스 메소드가 RX에서는 이렇게 쓰인다.
+        cellData.asDriver(onErrorJustReturn: []).drive(self.rx.items) {
+            tv, row, data in
+            let index = IndexPath(row: row, section: 0)
+            let cell = tv.dequeueReusableCell(withIdentifier: "BlogListCell", for: index) as! BlogListCell
+            cell.setData(data)
+            return cell
+        }
+        .disposed(by: disposeBag)
+        // 여기 까지 셀포 로우엣 대신해서 쓴다.
+    }
+    
+    private func attribute() {
+        self.backgroundColor = .white
+        self.register(BlogListCell.self, forCellReuseIdentifier: "BlogListCell")
+        self.separatorStyle = .singleLine
+        self.rowHeight = 100
+        self.tableHeaderView = headerView
+    }
+    
+    private func layout() {
+        
+    }
+}
